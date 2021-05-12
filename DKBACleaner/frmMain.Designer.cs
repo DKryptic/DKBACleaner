a6721d58a28f1cf3b2b7369bfdc2b3abab268747
Dennis <mrdennis87@gmail.com>
2021-05-12 14:47:55 -0400

    Adding start of deleting the directories, and killing the required tasks.

--- DKBACleaner/frmMain.Designer.cs
+++ DKBACleaner/frmMain.Designer.cs
@@ -29,20 +29,146 @@ namespace DKBACleaner
         /// </summary>
         private void InitializeComponent()
         {
+            this.btn_CleanTraces = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.RichTextBox();
+            this.txtModernWarfareDirectory = new System.Windows.Forms.RichTextBox();
+            this.btnSetMWDirectory = new System.Windows.Forms.Button();
+            this.lblModernWarfareDirectoryDesc = new System.Windows.Forms.Label();
+            this.lblSetColdWarDirectoryDesc = new System.Windows.Forms.Label();
+            this.btnSetCWDirectory = new System.Windows.Forms.Button();
+            this.txtColdWarDirectory = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
+            // 
+            // btn_CleanTraces
+            // 
+            this.btn_CleanTraces.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btn_CleanTraces.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btn_CleanTraces.FlatAppearance.BorderSize = 3;
+            this.btn_CleanTraces.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_CleanTraces.Location = new System.Drawing.Point(12, 16);
+            this.btn_CleanTraces.Name = "btn_CleanTraces";
+            this.btn_CleanTraces.Size = new System.Drawing.Size(503, 39);
+            this.btn_CleanTraces.TabIndex = 0;
+            this.btn_CleanTraces.Text = "Clean My Machine!";
+            this.btn_CleanTraces.UseVisualStyleBackColor = false;
+            this.btn_CleanTraces.Click += new System.EventHandler(this.btn_CleanTraces_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.BackColor = System.Drawing.Color.Black;
+            this.txtLog.ForeColor = System.Drawing.Color.White;
+            this.txtLog.Location = new System.Drawing.Point(12, 218);
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(503, 118);
+            this.txtLog.TabIndex = 1;
+            this.txtLog.Text = "";
+            // 
+            // txtModernWarfareDirectory
+            // 
+            this.txtModernWarfareDirectory.BackColor = System.Drawing.Color.Black;
+            this.txtModernWarfareDirectory.ForeColor = System.Drawing.Color.White;
+            this.txtModernWarfareDirectory.Location = new System.Drawing.Point(12, 158);
+            this.txtModernWarfareDirectory.Name = "txtModernWarfareDirectory";
+            this.txtModernWarfareDirectory.Size = new System.Drawing.Size(503, 23);
+            this.txtModernWarfareDirectory.TabIndex = 2;
+            this.txtModernWarfareDirectory.Text = "";
+            // 
+            // btnSetMWDirectory
+            // 
+            this.btnSetMWDirectory.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnSetMWDirectory.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnSetMWDirectory.FlatAppearance.BorderSize = 0;
+            this.btnSetMWDirectory.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSetMWDirectory.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSetMWDirectory.Location = new System.Drawing.Point(328, 187);
+            this.btnSetMWDirectory.Name = "btnSetMWDirectory";
+            this.btnSetMWDirectory.Size = new System.Drawing.Size(187, 22);
+            this.btnSetMWDirectory.TabIndex = 3;
+            this.btnSetMWDirectory.Text = "... Set Modern Warfare Directory ...";
+            this.btnSetMWDirectory.UseVisualStyleBackColor = false;
+            // 
+            // lblModernWarfareDirectoryDesc
+            // 
+            this.lblModernWarfareDirectoryDesc.AutoSize = true;
+            this.lblModernWarfareDirectoryDesc.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblModernWarfareDirectoryDesc.Location = new System.Drawing.Point(12, 142);
+            this.lblModernWarfareDirectoryDesc.Name = "lblModernWarfareDirectoryDesc";
+            this.lblModernWarfareDirectoryDesc.Size = new System.Drawing.Size(132, 13);
+            this.lblModernWarfareDirectoryDesc.TabIndex = 4;
+            this.lblModernWarfareDirectoryDesc.Text = "Modern Warfare Directory:";
+            // 
+            // lblSetColdWarDirectoryDesc
+            // 
+            this.lblSetColdWarDirectoryDesc.AutoSize = true;
+            this.lblSetColdWarDirectoryDesc.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSetColdWarDirectoryDesc.Location = new System.Drawing.Point(12, 75);
+            this.lblSetColdWarDirectoryDesc.Name = "lblSetColdWarDirectoryDesc";
+            this.lblSetColdWarDirectoryDesc.Size = new System.Drawing.Size(99, 13);
+            this.lblSetColdWarDirectoryDesc.TabIndex = 7;
+            this.lblSetColdWarDirectoryDesc.Text = "Cold War Directory:";
+            // 
+            // btnSetCWDirectory
+            // 
+            this.btnSetCWDirectory.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnSetCWDirectory.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnSetCWDirectory.FlatAppearance.BorderSize = 0;
+            this.btnSetCWDirectory.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSetCWDirectory.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSetCWDirectory.Location = new System.Drawing.Point(328, 120);
+            this.btnSetCWDirectory.Name = "btnSetCWDirectory";
+            this.btnSetCWDirectory.Size = new System.Drawing.Size(187, 22);
+            this.btnSetCWDirectory.TabIndex = 6;
+            this.btnSetCWDirectory.Text = "... Set Cold War Directory ...";
+            this.btnSetCWDirectory.UseVisualStyleBackColor = false;
+            this.btnSetCWDirectory.Click += new System.EventHandler(this.btnSetCWDirectory_Click);
+            // 
+            // txtColdWarDirectory
+            // 
+            this.txtColdWarDirectory.BackColor = System.Drawing.Color.Black;
+            this.txtColdWarDirectory.ForeColor = System.Drawing.Color.White;
+            this.txtColdWarDirectory.Location = new System.Drawing.Point(12, 91);
+            this.txtColdWarDirectory.Name = "txtColdWarDirectory";
+            this.txtColdWarDirectory.Size = new System.Drawing.Size(503, 23);
+            this.txtColdWarDirectory.TabIndex = 5;
+            this.txtColdWarDirectory.Text = "";
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(528, 348);
+            this.Controls.Add(this.lblSetColdWarDirectoryDesc);
+            this.Controls.Add(this.btnSetCWDirectory);
+            this.Controls.Add(this.txtColdWarDirectory);
+            this.Controls.Add(this.lblModernWarfareDirectoryDesc);
+            this.Controls.Add(this.btnSetMWDirectory);
+            this.Controls.Add(this.txtModernWarfareDirectory);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btn_CleanTraces);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "frmMain";
+            this.Opacity = 0.9D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "DKBACleaner";
+            this.Load += new System.EventHandler(this.frmMain_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_CleanTraces;
+        private System.Windows.Forms.RichTextBox txtLog;
+        private System.Windows.Forms.RichTextBox txtModernWarfareDirectory;
+        private System.Windows.Forms.Button btnSetMWDirectory;
+        private System.Windows.Forms.Label lblModernWarfareDirectoryDesc;
+        private System.Windows.Forms.Label lblSetColdWarDirectoryDesc;
+        private System.Windows.Forms.Button btnSetCWDirectory;
+        private System.Windows.Forms.RichTextBox txtColdWarDirectory;
     }
 }
 
--- DKBACleaner/frmMain.cs
+++ DKBACleaner/frmMain.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +15,225 @@ namespace DKBACleaner
 {
     public partial class frmMain : Form
     {
+        private const int ISUCCESSFUL = 0;
+        private const int IFAILED = 999;
+
+        private bool bIs64Bit = false;
+
+        private string[] directories = new string[10000];
+
+        private string sModernWarfareDirectory;
+        private string sColdWarDirectory;
+        private string sGameDirectory;
+
         public frmMain()
         {
             InitializeComponent();
+        }
+
+        private int fCleanTraces()
+        {
+            return IFAILED;
+        }
+
+        private int fCleanRegistry()
+        {
+            if (bIs64Bit)
+            {
+                RegistryKey rkLMBlizzardEntertainment64Bit = Registry.LocalMachine.OpenSubKey("Software\\WOW6432Node\\Blizzard Entertainment");
+                if (rkLMBlizzardEntertainment64Bit == null)
+                {
+                    AddToLog("Traces from registry have already cleaned.");
+                    return ISUCCESSFUL;
+                }
+
+                if (rkLMBlizzardEntertainment64Bit.SubKeyCount > 0)
+                {
+                    AddToLog("Found traces in the registry. Starting cleaning process.");
+
+                    rkLMBlizzardEntertainment64Bit.DeleteSubKeyTree("Battle.net");
+
+                    if (rkLMBlizzardEntertainment64Bit.SubKeyCount > 0)
+                    {
+                        AddToLog("Traces weren't able to be cleaned from the registry!");
+                    }
+                    else if (rkLMBlizzardEntertainment64Bit.SubKeyCount == 0)
+                    {
+                        AddToLog("Traces successfully deleted from the registry.");
+                        return ISUCCESSFUL;
+                    }
+                }
+            } else
+            {
+                RegistryKey rkLMBlizzardEntertainment32Bit = Registry.CurrentUser.OpenSubKey("Software\\Blizzard Entertainment");
+                if (rkLMBlizzardEntertainment32Bit == null)
+                {
+                    AddToLog("Traces from registry have already cleaned.");
+                    return ISUCCESSFUL;
+                }
+
+                if (rkLMBlizzardEntertainment32Bit.SubKeyCount > 0)
+                {
+                    AddToLog("Found traces in the registry. Starting cleaning process.");
+
+                    rkLMBlizzardEntertainment32Bit.DeleteSubKeyTree("Battle.net");
+
+                    if (rkLMBlizzardEntertainment32Bit.SubKeyCount > 0)
+                    {
+                        AddToLog("Traces weren't able to be cleaned from the registry!");
+                    }
+                    else if (rkLMBlizzardEntertainment32Bit.SubKeyCount == 0)
+                    {
+                        AddToLog("Traces successfully deleted from the registry.");
+                        return ISUCCESSFUL;
+                    }
+                }
+            }
+
+            return IFAILED;
+        }
+
+        private int fKillTasks()
+        {
+            Process[] ps = Process.GetProcessesByName("IM Agent.exe");
+            foreach (Process p in ps)
+                p.Kill();
+
+            Process[] ps2 = Process.GetProcessesByName("IM Battle.exe");
+            foreach (Process p in ps)
+                p.Kill();
+
+            // Need to verify processes are actually closed still..
+
+            return ISUCCESSFUL;
+        }
+
+
+        private int fCleanFiles()
+        {
+            for (int i = 0; i < directories.Length; i++) {
+                if (Directory.Exists(directories[i].ToString()))
+                {
+                    AddToLog("Deleting directory: " + i.ToString());
+                }
+            }
+
+            for (int j=0; j < directories.Length; j++)
+            {
+                if (Directory.Exists(directories[j].ToString()))
+                {
+                    AddToLog("Unable to deleted directory: " + j.ToString());
+                }
+            }
+            return IFAILED;
+        }
+
+        public void AddToLog(String sText)
+        {
+            try
+            {
+                // I created this function to make it easier to log things.
+                // This also scrolls automatically to the bottom of the text box when adding new text. User experience FTW?
+                txtLog.AppendText("\n" + sText);
+                txtLog.ScrollToCaret();
+            }
+            catch (Exception ex)
+            {
+                txtLog.Text += "Error adding to log: " + ex.ToString();
+            }
+        }
+
+        private void btn_CleanTraces_Click(object sender, EventArgs e)
+        {
+            if (txtColdWarDirectory.Text == "" && txtModernWarfareDirectory.Text == "")
+            {
+                MessageBox.Show("You are required to select at least of the games directories.");
+            }
+            else
+            {
+                //fCleanRegistry();
+                fKillTasks();
+                //fCleanFiles();
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            bIs64Bit = Environment.Is64BitOperatingSystem;
+
+            if (bIs64Bit)
+                AddToLog("Detected current OS as 64 bit.");
+            else
+                AddToLog("Detected current OS as 32 bit.");
+
+            directories.Append("C:\\Users\\DJr\\AppData\\Roaming\\Battle.net");
+            directories.Append("%localappdata%\\Activision\\Call Of Duty Black Ops Cold War");
+            directories.Append("%localappdata%\\Blizzard Entertainment");
+            directories.Append("%appdata%\\Battle.net");
+            directories.Append("%programdata%\\Activision");
+            directories.Append("%programdata%\\Battle.net");
+            directories.Append("%programdata%\\Blizzard Entertainment");
+            directories.Append("%USERPROFILE%\\Documents\\Call of Duty Modern Warfare");
+            directories.Append("%USERPROFILE%\\Documents\\Call Of Duty Black Ops Cold War");
+
+            if (Directory.Exists("%USERPROFILE%\\Documents\\Call of Duty Modern Warfare"))
+                txtModernWarfareDirectory.Text = "%USERPROFILE%\\Documents\\Call of Duty Modern Warfare";
+
+            if (Directory.Exists("%USERPROFILE%\\Documents\\Call Of Duty Black Ops Cold War"))
+                txtColdWarDirectory.Text = "%USERPROFILE%\\Documents\\Call Of Duty Black Ops Cold War";
+
+        }
+
+        private void DeleteDirectory(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                foreach (string file in Directory.GetFiles(path))
+                {
+                    File.Delete(file);
+                }
+                foreach (string directory in Directory.GetDirectories(path))
+                {
+                    DeleteDirectory(directory);
+                }
+                Directory.Delete(path);
+            }
+        }
+
+        private void btnSetCWDirectory_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                txtColdWarDirectory.Text = fbd.SelectedPath;
+            }
+            else
+            {
+                txtColdWarDirectory.Text = "No directory selected...";
+            }
+        }
+
+        private void fSaveCWDirectoryToRegistry()
+        {
+
+        }
+
+        private void fSaveMWDirectoryToRegistry()
+        {
+
+        }
+
+        private string fGetCWDirectoryFromRegistry()
+        {
+            string sCWDir = "";
+
+            return sCWDir;
+        }
+
+        private string fGetMWDirectoryFromRegistry()
+        {
+            string sMWDir = "";
+            return sMWDir;
         }
     }
 }
